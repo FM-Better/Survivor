45d6e1a23de94ca5bc4be4dd55c625264861ee9d
LAPTOP-FG35BCEI\FM <2412760825@qq.com>
2024-03-11 16:35:34 +0800

    添加经验升级系统的依赖

--- Assets/Scripts/System/ExpUpgrade/ExpUpgradeSystem.cs
+++ Assets/Scripts/System/ExpUpgrade/ExpUpgradeSystem.cs
@@ -17,13 +17,73 @@ namespace Survivor
         
         protected override void OnInit()
         {
-            Add(new ExpUpgradeItem()
+            var simpleDamage1 = Add(new ExpUpgradeItem()
                 .WithKey("SimpleAbilityDamage_Lv1")
                 .WithDescription("简单能力伤害提升1.5倍 Lv1")
                 .OnUpgrade((item) =>
                 {
                     Global.SimpleAbilityDamage.Value *= 1.5f;
                 }));
+            
+            var simpleDamage2 = Add(new ExpUpgradeItem()
+                .WithKey("SimpleAbilityDamage_Lv1")
+                .WithDescription("简单能力伤害提升1.5倍 Lv2")
+                .OnUpgrade((item) =>
+                {
+                    Global.SimpleAbilityDamage.Value *= 1.5f;
+                }))
+                .WithCondition((_) => simpleDamage1.UpgradeFinished);
+            simpleDamage1.OnChanged.Register(() =>
+            {
+                simpleDamage2.OnChanged.Trigger();
+            });
+            
+            var simpleDamage3 = Add(new ExpUpgradeItem()
+                .WithKey("SimpleAbilityDamage_Lv1")
+                .WithDescription("简单能力伤害提升1.5倍 Lv3")
+                .OnUpgrade((item) =>
+                {
+                    Global.SimpleAbilityDamage.Value *= 1.5f;
+                }))
+                .WithCondition((_) => simpleDamage2.UpgradeFinished);
+            simpleDamage2.OnChanged.Register(() =>
+            {
+                simpleDamage3.OnChanged.Trigger();
+            });
+
+            var simpleCD1 = Add(new ExpUpgradeItem()
+                .WithKey("SimpleAbilityCD_Lv1")
+                .WithDescription("简单能力间隔时间缩短0.5倍 Lv1")
+                .OnUpgrade((item) =>
+                {
+                    Global.SimpleAbilityCD.Value *= 0.5f;
+                }));
+            
+            var simpleCD2 = Add(new ExpUpgradeItem()
+                    .WithKey("SimpleAbilityCD_Lv2")
+                    .WithDescription("简单能力间隔时间缩短0.5倍 Lv2")
+                    .OnUpgrade((item) =>
+                    {
+                        Global.SimpleAbilityCD.Value *= 0.5f;
+                    }))
+                .WithCondition((_) => simpleDamage1.UpgradeFinished);
+            simpleCD1.OnChanged.Register(() =>
+            {
+                simpleCD2.OnChanged.Trigger();
+            });
+            
+            var simpleCD3 = Add(new ExpUpgradeItem()
+                    .WithKey("SimpleAbilityCD_Lv3")
+                    .WithDescription("简单能力间隔时间缩短0.5倍 Lv3")
+                    .OnUpgrade((item) =>
+                    {
+                        Global.SimpleAbilityCD.Value *= 0.5f;
+                    }))
+                .WithCondition((_) => simpleDamage2.UpgradeFinished);
+            simpleCD2.OnChanged.Register(() =>
+            {
+                simpleCD3.OnChanged.Trigger();
+            });
         }
     }
 }
